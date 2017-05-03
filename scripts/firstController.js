54591db7de0b79b663852cf89612c7ba24d15f02
Bryan Falls <bfalls1587@gmail.com>
2017-05-03 09:37:09 -0400

    fixed multi-word styling and functionality

--- scripts/firstController.js
+++ scripts/firstController.js
@@ -1,7 +1,7 @@
 app.controller('firstController', function($scope){
 	$scope.currentWords = [];
 
-	$scope.subjects = [
+	var subjects = [
 	"man", "mountain", "state", "ocean", "daddy", "mommy",
 	"country", "building", "cat", "dog", "Walt Disney", "barney", "love",
 	"happy", "plant", "bike", "ice cream", "face", "clock",
@@ -9,7 +9,7 @@ app.controller('firstController', function($scope){
 	"grandpa", "cousin"
 	];
 
-	$scope.adjectives = [
+	var adjectives = [
 	"good", "new", "first", "last", "long",
 	"great", "little", "own", "other", "old",
 	"right", "big", "high", "different", "small",
@@ -17,50 +17,50 @@ app.controller('firstController', function($scope){
 	"few", "public", "bad", "same", "able"
 	];
 
-	$scope.verbs = [
+	var verbs = [
 	"is", "are", "was", "were", "sound", "feels", "seems",
 	"tastes", "smell"
 	];
 
-	$scope.fancy = [
+	var fancy = [
 	"underline", "pink","circle",
 	 "red", "green", "blue",
 	"bubble", "shadow", "large", "small",
 	"dotted"
 	];
 
-	$scope.nextWord = 0;
+	var = nextWord = 0;
 
 	$scope.addWord = function(x, b) {
 		if (b) {
 			for(var i = 0; i < x; i++) {
-				if($scope.nextWord === 0){
-					$scope.currentWords.push({text: $scope.subjects[Math.floor(Math.random()*$scope.subjects.length)], isFancy: $scope.getRandom()});					$scope.nextWord++;
-				} else if ($scope.nextWord === 1) {
-					$scope.currentWords.push({text: $scope.verbs[Math.floor(Math.random()*$scope.verbs.length)], isFancy: $scope.getRandom()});
-					$scope.nextWord++;
-				} else {
-					$scope.currentWords.push({text: $scope.adjectives[Math.floor(Math.random()*$scope.adjectives.length)], isFancy: $scope.getRandom()});
-					$scope.nextWord = 0;
+				if(nextWord === 0){
+					$scope.currentWords.push({text: subjects[Math.floor(Math.random()*subjects.length)], isFancy: $scope.getRandom()});					nextWord++;
+				} else if (nextWord === 1) {
+					$scope.currentWords.push({text: verbs[Math.floor(Math.random()*verbs.length)], isFancy: $scope.getRandom()});
+					nextWord++;
+				} else if (nextWord === 2){
+					$scope.currentWords.push({text: adjectives[Math.floor(Math.random()*adjectives.length)], isFancy: $scope.getRandom()});
+					nextWord = 0;
 				};
 			};
 		} else {
 			for(var i = 0; i < x; i++) {
-				if ($scope.nextWord === 0){
-					$scope.currentWords.push({text: $scope.subjects[Math.floor(Math.random()*$scope.subjects.length)], isFancy: "none"});
-					$scope.nextWord++;
-				} else if ($scope.nextWord === 1){
-					$scope.currentWords.push({text: $scope.verbs[Math.floor(Math.random()*$scope.verbs.length)], isFancy: "none"});
-					$scope.nextWord++;
-				} else {
-					$scope.currentWords.push({text: $scope.adjectives[Math.floor(Math.random()*$scope.adjectives.length)], isFancy: "none"});
-					$scope.nextWord === 0;
+				if (nextWord === 0){
+					$scope.currentWords.push({text: subjects[Math.floor(Math.random()*subjects.length)], isFancy: "none"});
+					nextWord++;
+				} else if (nextWord === 1){
+					$scope.currentWords.push({text: verbs[Math.floor(Math.random()*verbs.length)], isFancy: "none"});
+					nextWord++;
+				} else if (nextWord === 2){
+					$scope.currentWords.push({text: adjectives[Math.floor(Math.random()*adjectives.length)], isFancy: "none"});
+					nextWord === 0;
 				};
 			};
 		};
 	};
 
 	$scope.getRandom = function(){
-		return $scope.fancy[Math.floor(Math.random()*$scope.fancy.length)];
+		return fancy[Math.floor(Math.random()*fancy.length)];
 	};
 });
